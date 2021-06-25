6a85f93250ee3f1210281b58d74066851260e223
jordiascension <jordiascension@enterpriseconsulting.es>
2021-06-25 10:48:38 +0200

    Initial commit with calculator example.

---
+++ MyFirstAppTests/CalculatorTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyFirstApp.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Add(2, 2) == 4);
+        }
+
+        [TestMethod()]
+        public void SubstractTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Substract(2, 2) == 0);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Multiply(2, 3) == 6);
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Divide(6, 3) == 2);
+        }
+    }
+}
